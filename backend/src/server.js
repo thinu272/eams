eceec40598dc768d4a7ccc4acc77b0d187b222b8
thinu272 <thinu.upadya@cbnit.lk>
2026-08-14 09:50:29 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -192,7 +192,7 @@ app.use('/api/upload', require('./routes/upload'));
 app.use('/api/devices', require('./routes/devices'));
 
 // --- DATABASE CONNECTION ---
-const MONGO_URI = process.env.MONGO_URI || "mongodb://eams_db_user:Fab3JzfDqeFXuZMN@ac-eibrjtr-shard-00-00.qsnrhfu.mongodb.net:27017,ac-eibrjtr-shard-00-01.qsnrhfu.mongodb.net:27017,ac-eibrjtr-shard-00-02.qsnrhfu.mongodb.net:27017/?ssl=true&replicaSet=atlas-lyu9mw-shard-0&authSource=admin&appName=Cluster0";
+const MONGO_URI = process.env.MONGODB_URI || process.env.MONGO_URI;
 
 mongoose
   .connect(MONGO_URI)
